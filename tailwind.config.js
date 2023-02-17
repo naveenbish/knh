3891561efb77f976bbb8e7c522521b1dd0802398
naveenbish <bishtnitin003@gmail.com>
2023-02-18 01:42:06 +0530

    More responsive then Before

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '580px',
+      'sm-x': '460px',
+      'md': '768px',
+      'lg': '900px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
